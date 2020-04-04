ecad0c252201363a0eba4b20f588a2c1831f4194
Shawn59 <xxx_respekt.ru@mail.ru>
2020-04-04 22:51:42 +0500

    #master - in process

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -3,17 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { LeftMenuComponent } from './left-menu/left-menu.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatStepperModule } from '@angular/material/stepper';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { AnalyseComponent } from './analyse/analyse.component';
+import { AnalyseSubStepperComponent } from './analyse-sub-stepper/analyse-sub-stepper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    LeftMenuComponent
+    AnalyseComponent,
+    AnalyseSubStepperComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        MatTabsModule,
+        BrowserAnimationsModule,
+        MatExpansionModule,
+        MatSliderModule,
+        MatStepperModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
